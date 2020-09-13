02c3f65bd7fb78e8bd7fb9f29ad016e6f5177bda
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-13 17:25:01 +0300

    Added IRepository

---
+++ EStudy/EStudy.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EStudy.Domain.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<string> CreateAsync(TEntity entity);
+        Task<string> EditAsync(TEntity entity);
+        Task<string> EditRangeAsync(IList<TEntity> entities);
+        Task<string> RemoveAsync(TEntity entity);
+        Task<string> RemoveRangeAsync(IList<TEntity> entities);
+        Task<TEntity> GetByWhereAsync(Expression<Func<TEntity, bool>> match);
+        Task<TEntity> GetByWhereAsTrackingAsync(Expression<Func<TEntity, bool>> match);
+        Task<List<TEntity>> GetListByWhereAsTrackingAsync(Expression<Func<TEntity, bool>> match);
+        Task<List<TEntity>> GetListByWhereAsync(Expression<Func<TEntity, bool>> match);
+        Task<List<TEntity>> GetListByWhereAsync(Expression<Func<TEntity, bool>> match, Expression<Func<TEntity, int, TEntity>> selector);
+        Task<List<TEntity>> GetListByWhereAsync(Expression<Func<TEntity, bool>> match, int count, int offset);
+        Task<List<TEntity>> GetListByWhereAsync(Expression<Func<TEntity, bool>> match, Expression<Func<TEntity, int, TEntity>> selector, int count, int offset);
+        Task<List<TEntity>> GetListByWhereAsync(Expression<Func<TEntity, bool>> match, Expression<Func<TEntity, object>> orderBy, bool OrderByAscending);
+        Task<List<TEntity>> GetListByWhereAsync(Expression<Func<TEntity, bool>> match, Expression<Func<TEntity, object>> orderBy, Expression<Func<TEntity, int, TEntity>> selector, bool OrderByAscending);
+        Task<List<TEntity>> GetListByWhereAsync(Expression<Func<TEntity, bool>> match, Expression<Func<TEntity, object>> orderBy, bool OrderByAscending, int count, int offset);
+        Task<List<TEntity>> GetListByWhereAsync(Expression<Func<TEntity, bool>> match, Expression<Func<TEntity, object>> orderBy, Expression<Func<TEntity, int, TEntity>> selector, bool OrderByAscending, int count, int offset);
+        Task<int> CountAsync();
+        Task<int> CountAsync(Expression<Func<TEntity, bool>> match);
+        Task<bool> IsExistAsync(Expression<Func<TEntity, bool>> match);
+        Task<string> SaveAsync();
+    }
+}
