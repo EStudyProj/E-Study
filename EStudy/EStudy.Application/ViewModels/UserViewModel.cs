0089ee04e78d29091f32ac6df5914d9cc2698cdf
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-13 17:40:44 +0300

    Added UserViewModel

---
+++ EStudy/EStudy.Application/ViewModels/UserViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace EStudy.Application.ViewModels
+{
+    public class UserViewModel
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Firstname { get; set; }
+    }
+}
