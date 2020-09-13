f630a29d8017601f7de3688a6c724b482aec1917
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-13 17:12:03 +0300

    Created DependencyContainer for register mvc and webapi services

---
+++ EStudy/EStudy.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace EStudy.Infrastructure.IoC
+{
+    public static class DependencyContainer
+    {
+        public static void RegisterMVCServices(this IServiceCollection services)
+        {
+
+        }
+
+        public static void RegisterAPIServices(this IServiceCollection services)
+        {
+
+        }
+    }
+}
