5cfb504d40d3d977de5e953fe4dee38ad2d5cf68
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-13 17:31:46 +0300

    Added content to EStudyContext

--- EStudy/EStudy.Infrastructure.Data/Context/EStudyContext.cs
+++ EStudy/EStudy.Infrastructure.Data/Context/EStudyContext.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using EStudy.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 namespace EStudy.Infrastructure.Data.Context
 {
-    public class EStudyContext
+    public class EStudyContext : DbContext
     {
+        public const string LocalConn = "Server=(localdb)\\MSSQLLocalDB;Database=EStudyDb;Trusted_Connection=True;";
+        public const string ServerConn = "";
+        public DbSet<User> Users { get; set; }
+        public EStudyContext(DbContextOptions options) : base(options) { }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(LocalConn);
+        }
     }
 }
