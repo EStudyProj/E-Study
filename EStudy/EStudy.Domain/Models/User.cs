4ad46c97377aa80c7cc3518125450ac77142bb3a
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-13 17:15:00 +0300

    Added User model

---
+++ EStudy/EStudy.Domain/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+namespace EStudy.Domain.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        [Required, MinLength(2), MaxLength(50)]
+        public string Firstname { get; set; }
+    }
+}
