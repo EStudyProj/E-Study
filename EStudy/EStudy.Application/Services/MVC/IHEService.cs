03c907dba866eae48fd66bc1448cb66c37f38891
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-15 00:12:37 +0300

    Added IHEService

---
+++ EStudy/EStudy.Application/Services/MVC/IHEService.cs
@@ -0,0 +1,18 @@
+﻿using EStudy.Application.Interfaces.MVC;
+using EStudy.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace EStudy.Application.Services.MVC
+{
+    public class IHEService : IIHEService
+    {
+        private readonly IIHERepository iheRepos;
+        public IHEService(IIHERepository _iheRepos)
+        {
+            iheRepos = _iheRepos;
+        }
+
+
+    }
+}
