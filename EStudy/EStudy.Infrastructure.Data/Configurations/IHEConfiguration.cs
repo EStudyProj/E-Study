44d88dfb458ee25717ad23322fb966c0c331ea8a
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-15 00:06:44 +0300

    Added IHEConfiguration

---
+++ EStudy/EStudy.Infrastructure.Data/Configurations/IHEConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using EStudy.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace EStudy.Infrastructure.Data.Configurations
+{
+    public class IHEConfiguration : IEntityTypeConfiguration<IHE>
+    {
+        public void Configure(EntityTypeBuilder<IHE> builder)
+        {
+            builder.HasKey(d => d.Id);
+            builder.HasIndex(d => new
+            {
+                d.Name,
+                d.ShortName,
+                d.StudentCode,
+                d.TeacherCode,
+                d.Region,
+                d.AreaIHE,
+                d.Locality,
+                d.LevelOfAccreditation
+            });
+        }
+    }
+}
