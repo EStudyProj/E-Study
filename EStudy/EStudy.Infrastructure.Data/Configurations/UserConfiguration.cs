71680ff1897a892e572e686bbf48c4567dbcff13
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-14 23:49:40 +0300

    Added UserConfiguration description

--- EStudy/EStudy.Infrastructure.Data/Configurations/UserConfiguration.cs
+++ EStudy/EStudy.Infrastructure.Data/Configurations/UserConfiguration.cs
@@ -1,10 +1,35 @@
-﻿using System;
+﻿using EStudy.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Text;
 namespace EStudy.Infrastructure.Data.Configurations
 {
-    public class UserConfiguration
+    public class UserConfiguration : IEntityTypeConfiguration<User>
     {
-
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(d => d.Id);
+            builder.HasIndex(d => new
+            {
+                d.Firstname,
+                d.Patronymic,
+                d.Lastname,
+                d.Username,
+                d.UserStatus,
+                d.Role,
+                d.Login,
+                d.Born,
+                d.Email,
+                d.Phone,
+                d.Chair,
+                d.LinkVerify,
+                d.StudentRecordBook,
+                d.StudentTicketNumber,
+                d.StudentTicketSeries,
+                d.TeacherCode
+            });
+        }
     }
 }
