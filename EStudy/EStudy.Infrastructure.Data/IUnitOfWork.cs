748fdfb64fcf87b79998e5fb00300c62df45a528
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-15 00:18:19 +0300

    Added IUnitOfWork

---
+++ EStudy/EStudy.Infrastructure.Data/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using EStudy.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EStudy.Infrastructure.Data
+{
+    public interface IUnitOfWork
+    {
+        IIHERepository IHEs { get; }
+        IUserRepository Users { get; }
+        Task<string> SaveAsync();
+    }
+}
