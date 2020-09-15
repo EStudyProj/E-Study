fdcdcd604d62cb760f6c81e7574f941b180abfb7
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-15 19:58:42 +0300

    Released method

--- EStudy/EStudy.Application/Interfaces/MVC/ITestService.cs
+++ EStudy/EStudy.Application/Interfaces/MVC/ITestService.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using EStudy.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+
 namespace EStudy.Application.Interfaces.MVC
 {
     public interface ITestService
     {
-
+        Task<TestEntity> GetAll();
     }
 }
--- EStudy/EStudy.Application/Services/MVC/TestService.cs
+++ EStudy/EStudy.Application/Services/MVC/TestService.cs
@@ -1,11 +1,24 @@
 ﻿using EStudy.Application.Interfaces.MVC;
+using EStudy.Domain.Models;
+using EStudy.Infrastructure.Data;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+
 namespace EStudy.Application.Services.MVC
 {
     public class TestService : ITestService
     {
+        private IUnitOfWork unitOfWork;
+        public TestService(IUnitOfWork _unitOfWork)
+        {
+            unitOfWork = _unitOfWork;
+        }
 
+        public async Task<TestEntity> GetAll()
+        {
+            return await unitOfWork.Tests.GetAllAsync();
+        }
     }
 }
