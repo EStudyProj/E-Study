378b09fafa98f4bfb36c0c3bf52baa6431625b59
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-15 23:16:27 +0300

    Added handler for get list of users

--- EStudy/EStudy.Application/Converter.cs
+++ EStudy/EStudy.Application/Converter.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using AutoMapper;
+using EStudy.Application.ViewModels.User;
+using EStudy.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 namespace EStudy.Application
 {
-    public class Converter
+    public class Converter : Profile
     {
-
+        public Converter()
+        {
+            CreateMap<User, UserViewModel>();
+            CreateMap<User, UserShortViewModel>();
+        }
     }
 }
--- EStudy/EStudy.Application/Services/MVC/UserService.cs
+++ EStudy/EStudy.Application/Services/MVC/UserService.cs
@@ -1,4 +1,5 @@
-﻿using EStudy.Application.Interfaces.MVC;
+﻿using AutoMapper;
+using EStudy.Application.Interfaces.MVC;
 using EStudy.Application.ViewModels;
 using EStudy.Application.ViewModels.User;
 using EStudy.Domain.Interfaces;
@@ -15,27 +16,29 @@ namespace EStudy.Application.Services.MVC
     public class UserService : IUserService
     {
         private readonly IUnitOfWork UnitOfWork;
-        public UserService(IUnitOfWork _UnitOfWork)
+        private readonly IMapper mapper;
+        public UserService(IUnitOfWork _UnitOfWork, IMapper _mapper)
         {
             UnitOfWork = _UnitOfWork;
+            mapper = _mapper;
         }
 
         public async Task<List<UserShortViewModel>> GetAllStudents()
         {
             var students = await UnitOfWork.Users.GetListByWhereAsync(d => d.UserStatus == UserStatus.Student);
-            return null;
+            return mapper.Map<List<UserShortViewModel>>(students);
         }
 
         public async Task<List<UserShortViewModel>> GetAllTeachers()
         {
-            var students = await UnitOfWork.Users.GetListByWhereAsync(d => d.UserStatus == UserStatus.Teacher);
-            return null;
+            var teachers = await UnitOfWork.Users.GetListByWhereAsync(d => d.UserStatus == UserStatus.Teacher);
+            return mapper.Map<List<UserShortViewModel>>(teachers);
         }
 
         public async Task<UserViewModel> GetUserById(int Id)
         {
-            var user = await UnitOfWork.Users.GetByWhereAsync(d => d.Id == Id);
-            return null;
+            var userdb = await UnitOfWork.Users.GetByWhereAsync(d => d.Id == Id);
+            return mapper.Map<UserViewModel>(userdb);
         }
     }
 }
