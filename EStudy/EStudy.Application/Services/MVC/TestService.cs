2748cd33726fd94b2d170ee6872ed93db3dae32c
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-15 19:57:00 +0300

    Added TestService and interface

---
+++ EStudy/EStudy.Application/Services/MVC/TestService.cs
@@ -0,0 +1,11 @@
+﻿using EStudy.Application.Interfaces.MVC;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace EStudy.Application.Services.MVC
+{
+    public class TestService : ITestService
+    {
+
+    }
+}
