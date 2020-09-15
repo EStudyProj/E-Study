1a61cf0d7093cdc8a7492cc2b1c9cc5d89f80714
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-16 00:02:26 +0300

    Added TestEntityViewModel

---
+++ EStudy/EStudy.Application/ViewModels/TestEntity/TestEntityViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EStudy.Application.ViewModels.TestEntity
+{
+    public class TestEntityViewModel
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Code { get; set; }
+        public string Guid { get; set; }
+    }
+}
