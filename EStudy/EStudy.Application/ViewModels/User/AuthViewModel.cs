78a7400f6e9c32f2293c96f1842d49dce645b771
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-16 19:33:17 +0300

    Some changed in AuthViewModel

--- EStudy/EStudy.Application/ViewModels/User/AuthViewModel.cs
+++ EStudy/EStudy.Application/ViewModels/User/AuthViewModel.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
-
 namespace EStudy.Application.ViewModels.User
 {
     public class AuthViewModel
     {
         [Required, MinLength(8), MaxLength(150)]
         public string Login { get; set; }
-        [Required, MinLength(8), MaxLength(300), DataType(DataType.Password)]
+        [Required, MinLength(8), MaxLength(75), DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
