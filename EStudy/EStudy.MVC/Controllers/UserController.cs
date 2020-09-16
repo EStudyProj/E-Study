088731f853cd2e9118b87c7951c31383c2dc5c2c
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-17 00:03:11 +0300

    Added some views

---
+++ EStudy/EStudy.MVC/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EStudy.Application.Interfaces.MVC;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EStudy.MVC.Controllers
+{
+    [Route("User")]
+    public class UserController : Controller
+    {
+        private readonly ILogger<UserController> _logger;
+        private IUserService userService;
+        public UserController(ILogger<UserController> logger, IUserService _userService)
+        {
+            _logger = logger;
+            userService = _userService;
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> GetUser(int Id)
+        {
+            var user = await userService.GetUserById(Id);
+            if (user == null)
+                return View("Error");
+            return View(user);
+        }
+    }
+}
