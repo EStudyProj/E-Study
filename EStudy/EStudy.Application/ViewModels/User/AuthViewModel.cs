a4776d108cbe8dc097428a6784db2f9e0a0158eb
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-16 19:28:58 +0300

    Added AuthViewModel

---
+++ EStudy/EStudy.Application/ViewModels/User/AuthViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EStudy.Application.ViewModels.User
+{
+    public class AuthViewModel
+    {
+        [Required, MinLength(8), MaxLength(150)]
+        public string Login { get; set; }
+        [Required, MinLength(8), MaxLength(300), DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
