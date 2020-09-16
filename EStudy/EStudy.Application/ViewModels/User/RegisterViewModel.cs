04e11cca83c2f6b2f83421ca8cef6bf67a808918
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-16 20:26:50 +0300

    Changed display name for mvc

--- EStudy/EStudy.Application/ViewModels/User/RegisterViewModel.cs
+++ EStudy/EStudy.Application/ViewModels/User/RegisterViewModel.cs
@@ -1,17 +1,18 @@
 ﻿using EStudy.Domain.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 namespace EStudy.Application.ViewModels.User
 {
     public class RegisterViewModel : EditModel
     {
-        [Required, MinLength(2), MaxLength(50)]
+        [Required, MinLength(2), MaxLength(50), DisplayName("Ім'я:")]
         public string Firstname { get; set; }
-        [Required, MinLength(3), MaxLength(50)]
+        [Required, MinLength(3), MaxLength(50), DisplayName("По-батькові:")]
         public string Patronymic { get; set; }
-        [Required, MinLength(2), MaxLength(50)]
+        [Required, MinLength(2), MaxLength(50), DisplayName("Прізвище:")]
         public string Lastname { get; set; }
         [Required, MinLength(8), MaxLength(150)]
         public string Login { get; set; }
