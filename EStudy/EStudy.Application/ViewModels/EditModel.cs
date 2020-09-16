c679b27277daa33cde4fc8cba791a06557e30f18
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-16 13:37:47 +0300

    Added EditModel

---
+++ EStudy/EStudy.Application/ViewModels/EditModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace EStudy.Application.ViewModels
+{
+    public class EditModel
+    {
+        public int Id { get; set; }
+        public int UserEditId { get; set; }
+        public string IPAddress { get; set; }
+    }
+}
--- EStudy/EStudy.Application/ViewModels/User/UserEditUsernameModel.cs
+++ EStudy/EStudy.Application/ViewModels/User/UserEditUsernameModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 namespace EStudy.Application.ViewModels.User
 {
-    public class UserEditUsernameModel
+    public class UserEditUsernameModel : EditModel
     {
         public string Username { get; set; }
     }
