67be1a4233f7e687db738d66398faa12165c275a
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-19 14:49:52 +0300

    Added IDepartmentService with some methods

---
+++ EStudy/EStudy.Application/Interfaces/MVC/IDepartmentService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EStudy.Application.Interfaces.MVC
+{
+    public interface IDepartmentService
+    {
+        Task<List<DepartmentViewModel>> GetDepartmentsByIHEId(int IheId);
+        Task<DepartmentViewModel> GetDepartmentById(int Id);
+    }
+}
