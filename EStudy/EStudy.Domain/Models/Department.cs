b32a7961482e4490368802e16219db903f4b17dd
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-19 14:39:04 +0300

    Added fields to full info Department entity

--- EStudy/EStudy.Domain/Models/Department.cs
+++ EStudy/EStudy.Domain/Models/Department.cs
@@ -8,5 +8,30 @@ namespace EStudy.Domain.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        [Required, MinLength(3), MaxLength(30)]
+        public string CreatedFromIpAddress { get; set; }
+        [Required]
+        public int CreatedByUserId { get; set; }
+        [Required, MinLength(4), MaxLength(24)]
+        public string Name { get; set; }
+        [MinLength(4), MaxLength(24)]
+        public string InstituteName { get; set; }
+        [Required, MinLength(2), MaxLength(100)]
+        public string Shifr { get; set; }
+        [Required]
+        public int HeadById { get; set; }
+        [Required, Phone]
+        public string Phone { get; set; }
+        [Required, MinLength(10), MaxLength(300)]
+        public string Description { get; set; }
+        public bool IsEdit { get; set; }
+        public DateTime DateLastEdit { get; set; }
+        public int? EditUserById { get; set; }
+        [MinLength(3), MaxLength(30)]
+        public string IpFromLastEdit { get; set; }
+        public int IHEId { get; set; }
+        public IHE IHE { get; set; }
     }
 }
