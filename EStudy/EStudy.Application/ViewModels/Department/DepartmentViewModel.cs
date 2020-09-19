7c72fca4d71f2a1443e15044378ace1671471704
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-19 14:51:35 +0300

    Added DepartmentViewModel

---
+++ EStudy/EStudy.Application/ViewModels/Department/DepartmentViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace EStudy.Application.ViewModels.Department
+{
+    public class DepartmentViewModel
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Name { get; set; }
+        public string InstituteName { get; set; }
+        public string Shifr { get; set; }
+        public int HeadById { get; set; }
+        public string Phone { get; set; }
+        public string Description { get; set; }
+        public bool IsEdit { get; set; }
+        public DateTime DateLastEdit { get; set; }
+        public IHEViewModel IHE { get; set; }
+    }
+}
