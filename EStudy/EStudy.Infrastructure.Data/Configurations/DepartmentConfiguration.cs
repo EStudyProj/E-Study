d0afbb5bcf7af1c745a5340c27857bc3e1d8f3e9
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-19 14:42:36 +0300

    Added DepartmentConfiguration

---
+++ EStudy/EStudy.Infrastructure.Data/Configurations/DepartmentConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using EStudy.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace EStudy.Infrastructure.Data.Configurations
+{
+    public class DepartmentConfiguration : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.HasKey(d => d.Id);
+            builder.HasIndex(d => new
+            {
+                d.IHEId,
+                d.Name,
+                d.HeadById,
+                d.InstituteName,
+                d.Phone,
+                d.Shifr
+            });
+        }
+    }
+}
