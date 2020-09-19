49c42b78c4bb620bb18faa8e25ca6b5a314b684d
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-19 12:47:23 +0300

    Added Department entity

---
+++ EStudy/EStudy.Domain/Models/Department.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+namespace EStudy.Domain.Models
+{
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
