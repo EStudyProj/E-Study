bdc5954487d8157ee0d71b9d7dd5df63e668faa7
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-21 12:07:55 +0300

    Added GroupCreateModel

---
+++ EStudy/EStudy.Application/ViewModels/Group/GroupCreateModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+namespace EStudy.Application.ViewModels.Group
+{
+    public class GroupCreateModel : EditModel
+    {
+        [Required, MinLength(2), MaxLength(20)]
+        public string GroupName { get; set; }
+        [Required]
+        public byte IndexItemToChanged { get; set; }
+        [MinLength(5), MaxLength(7)]
+        public string CodeForConnect { get; set; }
+        public int SpecialtyId { get; set; }
+    }
+}
