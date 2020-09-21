4024518bea81d78a9d661b8328f1d47e0042faea
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-21 12:11:56 +0300

    Added some methods to ISpecialtyService

--- EStudy/EStudy.Application/Interfaces/MVC/ISpecialtyService.cs
+++ EStudy/EStudy.Application/Interfaces/MVC/ISpecialtyService.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using EStudy.Application.ViewModels.Specialty;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+
 namespace EStudy.Application.Interfaces.MVC
 {
     public interface ISpecialtyService
     {
-
+        Task<string> CreateSpecialty(SpecialtyCreateModel model);
+        Task<SpecialtyViewModel> GetSpecialtyById(int Id);
+        Task<List<SpecialtyViewModel>> GetSpecialtiesByName(string name);
     }
 }
