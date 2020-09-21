d8512d2a7061a3b7328efd7994bbec841817dc06
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-21 15:16:08 +0300

    Added GroupController

---
+++ EStudy/EStudy.MVC/Controllers/GroupController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EStudy.Application.Interfaces.MVC;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EStudy.MVC.Controllers
+{
+    public class GroupController : Controller
+    {
+        private readonly ILogger<GroupController> _logger;
+        private IGroupService groupService;
+        public GroupController(ILogger<GroupController> logger, IGroupService _groupService)
+        {
+            _logger = logger;
+            groupService = _groupService;
+        }
+
+
+    }
+}
