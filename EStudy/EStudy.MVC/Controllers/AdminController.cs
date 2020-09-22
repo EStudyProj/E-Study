7483eb97b9eb14f4e23f6980107ed71abf9cada7
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-22 23:41:07 +0300

    Added ctor with logger

--- EStudy/EStudy.MVC/Controllers/AdminController.cs
+++ EStudy/EStudy.MVC/Controllers/AdminController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace EStudy.MVC.Controllers
 {
     public class AdminController : Controller
     {
-
+        private ILogger<AdminController> logger;
+        public AdminController(ILogger<AdminController> _logger)
+            => logger = _logger;
     }
 }
