feb1f60f1f5750c9307a72919ed8ee7588948ac1
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-24 23:07:50 +0300

    Added some methods to IAdminService

--- EStudy/EStudy.Application/Interfaces/MVC/IAdminService.cs
+++ EStudy/EStudy.Application/Interfaces/MVC/IAdminService.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using EStudy.Application.ViewModels.Group;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+
 namespace EStudy.Application.Interfaces.MVC
 {
     public interface IAdminService
     {
-
+        Task<GroupViewModel> GetFullInfoByGroupId(int Id);
+        Task<GroupViewModel> GetFullInfoByIHEId(int Id);
+        Task<GroupViewModel> GetFullInfoByUserId(int Id);
+        Task<GroupViewModel> GetFullInfoBySpecialtyId(int Id);
+        Task<GroupViewModel> GetFullInfoByDepartmentId(int Id);
     }
 }
