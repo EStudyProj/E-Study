f7f63e6eb5692935eb2df3c17d1756510c3d9576
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-25 18:21:42 +0300

    Added GroupSubjectConfiguration

---
+++ EStudy/EStudy.Infrastructure.Data/Configurations/GroupSubjectConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using EStudy.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace EStudy.Infrastructure.Data.Configurations
+{
+    public class GroupSubjectConfiguration : IEntityTypeConfiguration<GroupSubject>
+    {
+        public void Configure(EntityTypeBuilder<GroupSubject> builder)
+        {
+            builder.HasKey(d => d.Id);
+            builder.HasIndex(d => new
+            {
+                d.GroupId,
+                d.SubjectId,
+                d.CreatedFromIPAddress
+            });
+        }
+    }
+}
