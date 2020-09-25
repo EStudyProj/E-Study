eab1223cfa62a65021c1501c47161cb94fcdffe5
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-25 16:47:34 +0300

    Added GroupSubject

---
+++ EStudy/EStudy.Domain/Models/GroupSubject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+namespace EStudy.Domain.Models
+{
+    public class GroupSubject
+    {
+        [Key]
+        public int Id { get; set; }
+        public int GroupId { get; set; }
+        public Group Group { get; set; }
+        public int SubjectId { get; set; }
+        public Subject Subject { get; set; }
+
+
+
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        [Required, MinLength(3), MaxLength(30)]
+        public string CreatedFromIPAddress { get; set; }
+        [Required]
+        public int CreatedByUserId { get; set; }
+        [Required]
+        public bool IsEdit { get; set; } = false;
+        public DateTime? DateLastEdit { get; set; }
+        [MinLength(3), MaxLength(30)]
+        public string EditedFromIPAddress { get; set; }
+        public int? LastEditedByUserId { get; set; }
+    }
+}
