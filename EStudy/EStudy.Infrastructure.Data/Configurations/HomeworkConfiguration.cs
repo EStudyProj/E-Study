1459183637e265de40eaf4472b8298bb5a7c76cf
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-25 20:24:04 +0300

    Added HomeworkConfiguration

---
+++ EStudy/EStudy.Infrastructure.Data/Configurations/HomeworkConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using EStudy.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace EStudy.Infrastructure.Data.Configurations
+{
+    public class HomeworkConfiguration : IEntityTypeConfiguration<Homework>
+    {
+        public void Configure(EntityTypeBuilder<Homework> builder)
+        {
+            builder.HasKey(d => d.Id);
+            builder.HasIndex(d => new
+            {
+                d.ComplateByStudentId,
+                d.IsComplate,
+                d.TaskId,
+                d.TeacherSetId
+            });
+        }
+    }
+}
