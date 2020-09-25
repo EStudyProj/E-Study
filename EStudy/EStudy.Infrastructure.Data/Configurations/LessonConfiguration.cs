ba8357dbe104f7fa3652a6c7af07c51c3265bb6c
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-25 20:26:24 +0300

    Added LessonConfiguration

---
+++ EStudy/EStudy.Infrastructure.Data/Configurations/LessonConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using EStudy.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace EStudy.Infrastructure.Data.Configurations
+{
+    public class LessonConfiguration : IEntityTypeConfiguration<Lesson>
+    {
+        public void Configure(EntityTypeBuilder<Lesson> builder)
+        {
+            builder.HasKey(d => d.Id);
+            builder.HasIndex(d => new
+            {
+                d.Theme,
+                d.Text,
+                d.DateLesson,
+                d.AssignedToTeacherId,
+                d.SubjectId
+            });
+        }
+    }
+}
