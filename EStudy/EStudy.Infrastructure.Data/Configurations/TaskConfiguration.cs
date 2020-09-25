80df9ebce397744c95da97d70fbd8e9616e6dfea
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-25 20:33:18 +0300

    Added TaskConfiguration

---
+++ EStudy/EStudy.Infrastructure.Data/Configurations/TaskConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using EStudy.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace EStudy.Infrastructure.Data.Configurations
+{
+    public class TaskConfiguration : IEntityTypeConfiguration<Task>
+    {
+        public void Configure(EntityTypeBuilder<Task> builder)
+        {
+            builder.HasKey(d => d.Id);
+            builder.HasIndex(d => new
+            {
+                d.Text,
+                d.MaxMarkByThisTask,
+                d.DueDate,
+                d.LessonId
+            });
+        }
+    }
+}
