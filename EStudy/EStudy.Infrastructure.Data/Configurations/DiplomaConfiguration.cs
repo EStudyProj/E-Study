b6df7fdfb369aed66dea7d1479766f70c7a43480
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-25 20:22:09 +0300

    Added DiplomaConfiguration

---
+++ EStudy/EStudy.Infrastructure.Data/Configurations/DiplomaConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using EStudy.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace EStudy.Infrastructure.Data.Configurations
+{
+    public class DiplomaConfiguration : IEntityTypeConfiguration<Diploma>
+    {
+        public void Configure(EntityTypeBuilder<Diploma> builder)
+        {
+            builder.HasKey(d => d.Id);
+            builder.HasIndex(d => new
+            {
+                d.IsRed,
+                d.InPass,
+                d.UniqCode,
+            });
+        }
+    }
+}
