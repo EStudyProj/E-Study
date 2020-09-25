15d5f168a2fb9188bae295f988f4841482bcf641
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-25 20:31:25 +0300

    Added MarkConfiguration

---
+++ EStudy/EStudy.Infrastructure.Data/Configurations/MarkConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using EStudy.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace EStudy.Infrastructure.Data.Configurations
+{
+    public class MarkConfiguration : IEntityTypeConfiguration<Mark>
+    {
+        public void Configure(EntityTypeBuilder<Mark> builder)
+        {
+            builder.HasKey(d => d.Id);
+            builder.HasIndex(d => new
+            {
+                d.Value,
+                d.TypeMark,
+                d.TeacherSetId,
+                d.IsHindFromStudents,
+                d.LessonId,
+                d.HomeworkId
+            });
+        }
+    }
+}
