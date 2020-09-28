ed3b1f12816f0433b720c39be4e339ea9eea4297
@Yaroslav08 <yaroslav.mudryk@gmail.com>
2020-09-28 15:02:29 +0300

    Added some repositories

---
+++ EStudy/EStudy.Infrastructure.Data/Repositories/HomeworkRepository.cs
@@ -0,0 +1,12 @@
+﻿using EStudy.Domain.Interfaces;
+using EStudy.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace EStudy.Infrastructure.Data.Repositories
+{
+    public class HomeworkRepository : Repository<Homework>, IHomeworkRepository
+    {
+
+    }
+}
