92487e8892b490b9718febee6f1c1bdfdac58fbf
@Yaroslav08 <yaroslav.mudryk@gmail.com>
2020-09-28 19:06:22 +0300

    Added some services and repositories to scoped

--- EStudy/EStudy.Infrastructure.IoC/DependencyContainer.cs
+++ EStudy/EStudy.Infrastructure.IoC/DependencyContainer.cs
@@ -14,35 +14,50 @@ namespace EStudy.Infrastructure.IoC
         public static void RegisterMVCServices(this IServiceCollection services)
         {
             services.AddScoped<IUnitOfWork, UnitOfWork>();
-            services.AddScoped<ITestEntityRepository, TestEntityRepository>();
-            services.AddScoped<ITestService, TestService>();
+            services.AddScoped<IAdminService, AdminService>();
 
 
-
-            services.AddScoped<IUserRepository, UserRepository>();
-            services.AddScoped<IUserService, UserService>();
-
-            services.AddScoped<IIHERepository, IHERepository>();
-            services.AddScoped<IIHEService, IHEService>();
 
             services.AddScoped<IDepartmentRepository, DepartmentRepository>();
             services.AddScoped<IDepartmentService, DepartmentService>();
 
-            services.AddScoped<ISpecialtyRepository, SpecialtyRepository>();
-            services.AddScoped<ISpecialtyService, SpecialtyService>();
+            services.AddScoped<IDiplomaRepository, DiplomaRepository>();
+            services.AddScoped<IDiplomaService, DiplomaService>();
+
+            services.AddScoped<IFileRepository, FileRepository>();
+            services.AddScoped<IFileService, FileService>();
 
             services.AddScoped<IGroupRepository, GroupRepository>();
             services.AddScoped<IGroupService, GroupService>();
 
+            services.AddScoped<IGroupSubjectRepository, GroupSubjectRepository>();
+
+            services.AddScoped<IHomeworkRepository, HomeworkRepository>();
+            services.AddScoped<IHomeworkService, HomeworkService>();
+
+            services.AddScoped<IIHERepository, IHERepository>();
+            services.AddScoped<IIHEService, IHEService>();
+
+            services.AddScoped<ILessonRepository, LessonRepository>();
+            services.AddScoped<ILessonService, LessonService>();
+
+            services.AddScoped<IMarkRepository, MarkRepository>();
+            services.AddScoped<IMarkService, MarkService>();
+
+            services.AddScoped<ISpecialtyRepository, SpecialtyRepository>();
+            services.AddScoped<ISpecialtyService, SpecialtyService>();
+
             services.AddScoped<ISubjectRepository, SubjectRepository>();
             services.AddScoped<ISubjectService, SubjectService>();
 
-            services.AddScoped<IGroupSubjectRepository, GroupSubjectRepository>();
+            services.AddScoped<ITaskRepository, TaskRepository>();
+            services.AddScoped<ITaskService, TaskService>();
 
-            services.AddScoped<IAdminService, AdminService>();
+            services.AddScoped<ITestEntityRepository, TestEntityRepository>();
+            services.AddScoped<ITestService, TestService>();
 
-            services.AddScoped<IDiplomaRepository, DiplomaRepository>();
-            services.AddScoped<IDiplomaService, DiplomaService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserService, UserService>();
         }
 
         public static void RegisterAPIServices(this IServiceCollection services)
