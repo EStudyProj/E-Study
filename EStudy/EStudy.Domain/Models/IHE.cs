31b9bdf796cb03d8468e32e3d07e3db0b0e3b3d2
@Yaroslav08 <yaroslav.mudryk@gmail.com>
2020-09-28 22:57:54 +0300

    Added EnglishName field to IHE entity

--- EStudy/EStudy.Domain/Models/IHE.cs
+++ EStudy/EStudy.Domain/Models/IHE.cs
@@ -11,6 +11,8 @@ namespace EStudy.Domain.Models
         public int Id { get; set; }
         [Required, MinLength(3), MaxLength(200)]
         public string Name { get; set; }
+        [MinLength(3), MaxLength(200)]
+        public string EnglishName { get; set; }
         [Required, MinLength(2), MaxLength(25)]
         public string ShortName { get; set; }
         [Required]
