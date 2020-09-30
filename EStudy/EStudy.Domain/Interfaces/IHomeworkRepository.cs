c2c7df388c5f23ccd904da8f28bd8e24c2aa28d2
@Yaroslav08 <yaroslav.mudryk@gmail.com>
2020-09-30 20:19:00 +0300

    Added method for getting homework with Task entity

--- EStudy/EStudy.Domain/Interfaces/IHomeworkRepository.cs
+++ EStudy/EStudy.Domain/Interfaces/IHomeworkRepository.cs
@@ -2,10 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+
 namespace EStudy.Domain.Interfaces
 {
     public interface IHomeworkRepository : IRepository<Homework>
     {
-
+        Task<Homework> GetHomeworkWithTaskById(int Id);
     }
 }
--- EStudy/EStudy.Infrastructure.Data/Repositories/HomeworkRepository.cs
+++ EStudy/EStudy.Infrastructure.Data/Repositories/HomeworkRepository.cs
@@ -1,12 +1,18 @@
 ﻿using EStudy.Domain.Interfaces;
 using EStudy.Domain.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+
 namespace EStudy.Infrastructure.Data.Repositories
 {
     public class HomeworkRepository : Repository<Homework>, IHomeworkRepository
     {
-
+        public async Task<Homework> GetHomeworkWithTaskById(int Id)
+        {
+            return await db.Homeworks.AsNoTracking().Include(d => d.Task).SingleOrDefaultAsync(d => d.Id == Id);
+        }
     }
 }
