992ce356a5997113a1f97155a56fb8315a636a7a
@Yaroslav08 <yaroslav.mudryk@gmail.com>
2020-09-30 20:34:14 +0300

    Added method for getting presence with Lesson entity

--- EStudy/EStudy.Domain/Interfaces/IPresenceRepository.cs
+++ EStudy/EStudy.Domain/Interfaces/IPresenceRepository.cs
@@ -2,11 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace EStudy.Domain.Interfaces
 {
     public interface IPresenceRepository : IRepository<Presence>
     {
-
+        Task<Presence> GetPresenceWithLessonById(long Id);
     }
 }
--- EStudy/EStudy.Infrastructure.Data/Repositories/PresenceRepository.cs
+++ EStudy/EStudy.Infrastructure.Data/Repositories/PresenceRepository.cs
@@ -1,12 +1,18 @@
 ﻿using EStudy.Domain.Interfaces;
 using EStudy.Domain.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+
 namespace EStudy.Infrastructure.Data.Repositories
 {
     public class PresenceRepository : Repository<Presence>, IPresenceRepository
     {
-
+        public async Task<Presence> GetPresenceWithLessonById(long Id)
+        {
+            return await db.Presences.AsNoTracking().Include(d => d.Lesson).SingleOrDefaultAsync(d => d.Id == Id);
+        }
     }
 }
