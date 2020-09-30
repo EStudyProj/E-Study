91a7f5c100e6b82b88ae5e761c68ede176662da1
@Yaroslav08 <yaroslav.mudryk@gmail.com>
2020-09-30 20:37:09 +0300

    Added method for getting specialty with Groups entities

--- EStudy/EStudy.Domain/Interfaces/ISpecialtyRepository.cs
+++ EStudy/EStudy.Domain/Interfaces/ISpecialtyRepository.cs
@@ -2,10 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+
 namespace EStudy.Domain.Interfaces
 {
     public interface ISpecialtyRepository : IRepository<Specialty>
     {
-
+        Task<Specialty> GetSpecialtyWithGroupsById(int Id);
     }
 }
--- EStudy/EStudy.Infrastructure.Data/Repositories/SpecialtyRepository.cs
+++ EStudy/EStudy.Infrastructure.Data/Repositories/SpecialtyRepository.cs
@@ -1,12 +1,18 @@
 ﻿using EStudy.Domain.Interfaces;
 using EStudy.Domain.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+
 namespace EStudy.Infrastructure.Data.Repositories
 {
     public class SpecialtyRepository : Repository<Specialty>, ISpecialtyRepository
     {
-
+        public async Task<Specialty> GetSpecialtyWithGroupsById(int Id)
+        {
+            return await db.Specialties.AsNoTracking().Include(d => d.Groups).SingleOrDefaultAsync(d => d.Id == Id);
+        }
     }
 }
