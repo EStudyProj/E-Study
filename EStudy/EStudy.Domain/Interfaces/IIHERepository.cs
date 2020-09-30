1cb20a7b2be7a928673733437ec45421d404e56c
@Yaroslav08 <yaroslav.mudryk@gmail.com>
2020-09-30 20:22:04 +0300

    Added method for getting IHE with Departments entity

--- EStudy/EStudy.Domain/Interfaces/IIHERepository.cs
+++ EStudy/EStudy.Domain/Interfaces/IIHERepository.cs
@@ -2,10 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+
 namespace EStudy.Domain.Interfaces
 {
     public interface IIHERepository : IRepository<IHE>
     {
-
+        Task<IHE> GetIHEWithDepartmentsById(int Id);
     }
 }
--- EStudy/EStudy.Infrastructure.Data/Repositories/IHERepository.cs
+++ EStudy/EStudy.Infrastructure.Data/Repositories/IHERepository.cs
@@ -1,10 +1,15 @@
 ﻿using EStudy.Domain.Interfaces;
 using EStudy.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace EStudy.Infrastructure.Data.Repositories
 {
     public class IHERepository : Repository<IHE>, IIHERepository
     {
-
+        public async Task<IHE> GetIHEWithDepartmentsById(int Id)
+        {
+            return await db.IHEs.AsNoTracking().Include(d => d.Departments).SingleOrDefaultAsync(d => d.Id == Id);
+        }
     }
 }
