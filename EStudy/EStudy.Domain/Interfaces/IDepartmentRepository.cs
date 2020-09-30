e4d53d6c660b36ebfc1513863d95364b6b99c41d
@Yaroslav08 <yaroslav.mudryk@gmail.com>
2020-09-30 20:15:38 +0300

    Added method for getting department with specialty

--- EStudy/EStudy.Domain/Interfaces/IDepartmentRepository.cs
+++ EStudy/EStudy.Domain/Interfaces/IDepartmentRepository.cs
@@ -2,10 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+
 namespace EStudy.Domain.Interfaces
 {
     public interface IDepartmentRepository : IRepository<Department>
     {
-
+        Task<Department> GetDepartmentWithSpecsById(int Id);
     }
 }
--- EStudy/EStudy.Infrastructure.Data/Repositories/DepartmentRepository.cs
+++ EStudy/EStudy.Infrastructure.Data/Repositories/DepartmentRepository.cs
@@ -1,12 +1,18 @@
 ﻿using EStudy.Domain.Interfaces;
 using EStudy.Domain.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+
 namespace EStudy.Infrastructure.Data.Repositories
 {
     public class DepartmentRepository : Repository<Department>, IDepartmentRepository
     {
-
+        public async Task<Department> GetDepartmentWithSpecsById(int Id)
+        {
+            return await db.Departments.AsNoTracking().Include(d => d.Specialties).SingleOrDefaultAsync(d => d.Id == Id);
+        }
     }
 }
