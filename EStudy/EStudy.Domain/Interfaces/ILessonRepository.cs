bc78c0ada9c2f117f7acbe814300eaabc0928d41
@Yaroslav08 <yaroslav.mudryk@gmail.com>
2020-09-30 20:27:11 +0300

    Added method for getting lesson with Subject entity

--- EStudy/EStudy.Domain/Interfaces/ILessonRepository.cs
+++ EStudy/EStudy.Domain/Interfaces/ILessonRepository.cs
@@ -2,10 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+
 namespace EStudy.Domain.Interfaces
 {
     public interface ILessonRepository : IRepository<Lesson>
     {
-
+        Task<Lesson> GetLessonWithSubjectById(int Id);
     }
 }
--- EStudy/EStudy.Infrastructure.Data/Repositories/LessonRepository.cs
+++ EStudy/EStudy.Infrastructure.Data/Repositories/LessonRepository.cs
@@ -1,12 +1,18 @@
 ﻿using EStudy.Domain.Interfaces;
 using EStudy.Domain.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+
 namespace EStudy.Infrastructure.Data.Repositories
 {
     public class LessonRepository : Repository<Lesson>, ILessonRepository
     {
-
+        public async Task<Lesson> GetLessonWithSubjectById(int Id)
+        {
+            return await db.Lessons.AsNoTracking().Include(d => d.Subject).SingleOrDefaultAsync(d => d.Id == Id);
+        }
     }
 }
