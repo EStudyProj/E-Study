fa051b5f4b67d4ae67b027b553dd7009d1d8c041
ZhdanDev <zhdancreator@gmail.com>
2020-10-01 10:53:26 +0300

    Added Schedule entity

---
+++ EStudy/EStudy.Domain/Models/Schedule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EStudy.Domain.Models
+{
+    public class Schedule
+    {
+        [Key]
+        public int Id { get; set; }
+        public string DayOfWeek { get; set; }
+        public int ClassId { get; set; }
+        public Сlass Class { get; set; }
+        public int GroupId { get; set; }
+        public Group Group { get; set; }
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+        public int TypeOfClassId { get; set; }
+        public TypeOfClass TypeOfClass { get; set; }
+        public int ClassRoomId { get; set; }
+        public ClassRoom ClassRoom { get; set; }
+    }
+}
