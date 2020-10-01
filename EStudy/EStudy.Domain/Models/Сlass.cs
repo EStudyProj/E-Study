a7022c710b9e5216b2e31bfacecb85f2e613711b
ZhdanDev <zhdancreator@gmail.com>
2020-10-01 10:37:46 +0300

    Added  "class" entity

---
+++ EStudy/EStudy.Domain/Models/Сlass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EStudy.Domain.Models
+{
+    public class Сlass//Пара
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int NumOfClass { get; set; }//номер пары
+        [Required]
+        public DateTime TimeOfStart { get; set; }
+        [Required]
+        public DateTime TimeOfEnd { get; set; }
+
+
+
+    }
+}
