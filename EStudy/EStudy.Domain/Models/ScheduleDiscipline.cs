2b73bf599111d46c77d858d57333920751875606
@Yaroslav08 <yaroslav.mudryk@gmail.com>
2020-10-02 15:50:20 +0300

    Added ScheduleDiscipline

---
+++ EStudy/EStudy.Domain/Models/ScheduleDiscipline.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+namespace EStudy.Domain.Models
+{
+    public class ScheduleDiscipline
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MinLength(3), MaxLength(100)]
+        public string DisciplineName { get; set; }
+        [MinLength(1), MaxLength(5)]
+        public string ShortDisciplineName { get; set; }
+        [MinLength(3), MaxLength(100)]
+        public string DisciplineNameEng { get; set; }
+    }
+}
