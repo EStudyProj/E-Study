7292ca0edb0a653948d37d3f52329389a300377f
@Yaroslav08 <yaroslav.mudryk@gmail.com>
2020-10-02 16:01:53 +0300

    Added ScheduleParityOfWeek

---
+++ EStudy/EStudy.Domain/Models/ScheduleParityOfWeek.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+namespace EStudy.Domain.Models
+{
+    public class ScheduleParityOfWeek
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MinLength(3), MaxLength(25)]
+        public string Week { get; set; }
+    }
+}
