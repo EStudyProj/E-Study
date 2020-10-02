bc8faa992e4fd0c1c40cbe6aaaffc86d15d6be2b
@Yaroslav08 <yaroslav.mudryk@gmail.com>
2020-10-02 15:28:51 +0300

    Added ScheduleLesson

---
+++ EStudy/EStudy.Domain/Models/ScheduleLesson.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+namespace EStudy.Domain.Models
+{
+    public class ScheduleLesson
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public byte Number { get; set; }
+        [Required]
+        public DateTime Start { get; set; }
+        [Required]
+        public DateTime End { get; set; }
+    }
+}
