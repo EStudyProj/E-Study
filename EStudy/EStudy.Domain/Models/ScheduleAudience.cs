0ca3f4f715d220580f8deee96c5134358a7db602
@Yaroslav08 <yaroslav.mudryk@gmail.com>
2020-10-02 15:35:57 +0300

    Added ScheduleAudience

---
+++ EStudy/EStudy.Domain/Models/ScheduleAudience.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+namespace EStudy.Domain.Models
+{
+    public class ScheduleAudience
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MinLength(1), MaxLength(25)]
+        public string NameAudience { get; set; }
+        [MinLength(1), MaxLength(25)]
+        public string NameAudienceEng { get; set; }
+    }
+}
