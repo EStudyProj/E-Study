25bbc529b1991c6965155f87aeaa67ef3fe1856a
@Yaroslav08 <yaroslav.mudryk@gmail.com>
2020-10-02 16:27:53 +0300

    Added Schedule

---
+++ EStudy/EStudy.Domain/Models/Schedule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+namespace EStudy.Domain.Models
+{
+    public class Schedule
+    {
+        [Key]
+        public long Id { get; set; }
+        [Required]
+        public bool IsReplacement { get; set; } = false;
+        public int? TeacherId { get; set; }
+        [Required]
+        public DateTime DateLesson { get; set; }
+        public int ScheduleDayOfWeekId { get; set; }
+        public ScheduleDayOfWeek ScheduleDayOfWeek { get; set; }
+        public int ScheduleParityOfWeekId { get; set; }
+        public ScheduleParityOfWeek ScheduleParityOfWeek { get; set; }
+        public int ScheduleLessonId { get; set; }
+        public ScheduleLesson ScheduleLesson { get; set; }
+        public int ScheduleGroupId { get; set; }
+        public ScheduleGroup ScheduleGroup { get; set; }
+        public int ScheduleDisciplineId { get; set; }
+        public ScheduleDiscipline ScheduleDiscipline { get; set; }
+        public int ScheduleTypeLessonId { get; set; }
+        public ScheduleTypeLesson ScheduleTypeLesson { get; set; }
+        public int ScheduleAudienceId { get; set; }
+        public ScheduleAudience ScheduleAudience { get; set; }
+    }
+}
