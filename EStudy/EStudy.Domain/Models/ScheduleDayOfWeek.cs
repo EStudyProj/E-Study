f51613716e32b00ffcae4872761bf1fb4de2a873
@Yaroslav08 <yaroslav.mudryk@gmail.com>
2020-10-02 16:03:41 +0300

    Added ScheduleDayOfWeek

---
+++ EStudy/EStudy.Domain/Models/ScheduleDayOfWeek.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+namespace EStudy.Domain.Models
+{
+    public class ScheduleDayOfWeek
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required, MinLength(3), MaxLength(25)]
+        public string Day { get; set; }
+        [MinLength(3), MaxLength(25)]
+        public string DayEng { get; set; }
+    }
+}
