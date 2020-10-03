b3f98126b6d306f18f92d4e0eea27057de36a357
@Yaroslav08 <yaroslav.mudryk@gmail.com>
2020-10-03 14:49:41 +0300

    Released crud methods from IScheduleService

--- EStudy/EStudy.Application/Services/MVC/ScheduleService.cs
+++ EStudy/EStudy.Application/Services/MVC/ScheduleService.cs
@@ -17,6 +17,138 @@ namespace EStudy.Application.Services.MVC
             uniOfWork = _uniOfWork;
         }
 
+        #region AddOperation
+        public async Task<string> AddScheduleAudience(ScheduleAudience model)
+        {
+            return await uniOfWork.ScheduleAudiences.CreateAsync(model);
+        }
+
+        public async Task<string> AddScheduleDayOfWeek(ScheduleDayOfWeek model)
+        {
+            return await uniOfWork.ScheduleDayOfWeeks.CreateAsync(model);
+        }
+
+        public async Task<string> AddScheduleDiscipline(ScheduleDiscipline model)
+        {
+            return await uniOfWork.ScheduleDisciplines.CreateAsync(model);
+        }
+
+        public async Task<string> AddScheduleGroup(ScheduleGroup model)
+        {
+            return await uniOfWork.ScheduleGroups.CreateAsync(model);
+        }
+
+        public async Task<string> AddScheduleLesson(ScheduleLesson model)
+        {
+            return await uniOfWork.ScheduleLessons.CreateAsync(model);
+        }
+
+        public async Task<string> AddScheduleParityOfWeek(ScheduleParityOfWeek model)
+        {
+            return await uniOfWork.ScheduleParityOfWeeks.CreateAsync(model);
+        }
+
+        public async Task<string> AddScheduleTypeLesson(ScheduleTypeLesson model)
+        {
+            return await uniOfWork.ScheduleTypeLessons.CreateAsync(model);
+        }
+        #endregion
+
+        #region EditOperation
+        public async Task<string> EditScheduleAudience(ScheduleAudience model)
+        {
+            return await uniOfWork.ScheduleAudiences.EditAsync(model);
+        }
+
+        public async Task<string> EditScheduleDayOfWeek(ScheduleDayOfWeek model)
+        {
+            return await uniOfWork.ScheduleDayOfWeeks.EditAsync(model);
+        }
+
+        public async Task<string> EditScheduleDiscipline(ScheduleDiscipline model)
+        {
+            return await uniOfWork.ScheduleDisciplines.EditAsync(model);
+        }
+
+        public async Task<string> EditScheduleGroup(ScheduleGroup model)
+        {
+            return await uniOfWork.ScheduleGroups.EditAsync(model);
+        }
+
+        public async Task<string> EditScheduleLesson(ScheduleLesson model)
+        {
+            return await uniOfWork.ScheduleLessons.EditAsync(model);
+        }
+
+        public async Task<string> EditScheduleParityOfWeek(ScheduleParityOfWeek model)
+        {
+            return await uniOfWork.ScheduleParityOfWeeks.EditAsync(model);
+        }
+
+        public async Task<string> EditScheduleTypeLesson(ScheduleTypeLesson model)
+        {
+            return await uniOfWork.ScheduleTypeLessons.EditAsync(model);
+        }
+        #endregion
+
+        #region RemoveOperation
+        public async Task<string> RemoveScheduleAudience(int Id)
+        {
+            var model = await uniOfWork.ScheduleAudiences.GetByWhereAsTrackingAsync(d => d.Id == Id);
+            if (model == null)
+                return "Not found";
+            return await uniOfWork.ScheduleAudiences.RemoveAsync(model);
+        }
+
+        public async Task<string> RemoveScheduleDayOfWeek(int Id)
+        {
+            var model = await uniOfWork.ScheduleDayOfWeeks.GetByWhereAsTrackingAsync(d => d.Id == Id);
+            if (model == null)
+                return "Not found";
+            return await uniOfWork.ScheduleDayOfWeeks.RemoveAsync(model);
+        }
+
+        public async Task<string> RemoveScheduleDiscipline(int Id)
+        {
+            var model = await uniOfWork.ScheduleDisciplines.GetByWhereAsTrackingAsync(d => d.Id == Id);
+            if (model == null)
+                return "Not found";
+            return await uniOfWork.ScheduleDisciplines.RemoveAsync(model);
+        }
+
+        public async Task<string> RemoveScheduleGroup(int Id)
+        {
+            var model = await uniOfWork.ScheduleGroups.GetByWhereAsTrackingAsync(d => d.Id == Id);
+            if (model == null)
+                return "Not found";
+            return await uniOfWork.ScheduleGroups.RemoveAsync(model);
+        }
+
+        public async Task<string> RemoveScheduleLesson(int Id)
+        {
+            var model = await uniOfWork.ScheduleLessons.GetByWhereAsTrackingAsync(d => d.Id == Id);
+            if (model == null)
+                return "Not found";
+            return await uniOfWork.ScheduleLessons.RemoveAsync(model);
+        }
+
+        public async Task<string> RemoveScheduleParityOfWeek(int Id)
+        {
+            var model = await uniOfWork.ScheduleParityOfWeeks.GetByWhereAsTrackingAsync(d => d.Id == Id);
+            if (model == null)
+                return "Not found";
+            return await uniOfWork.ScheduleParityOfWeeks.RemoveAsync(model);
+        }
+
+        public async Task<string> RemoveScheduleTypeLesson(int Id)
+        {
+            var model = await uniOfWork.ScheduleTypeLessons.GetByWhereAsTrackingAsync(d => d.Id == Id);
+            if (model == null)
+                return "Not found";
+            return await uniOfWork.ScheduleTypeLessons.RemoveAsync(model);
+        }
+        #endregion
+
 
         public async Task<Schedule> GetScheduleById(long Id)
         {
