b3e88d713eee4d41e24e02e2502c54e9aa4751e2
@Yaroslav08 <yaroslav.mudryk@gmail.com>
2020-10-03 14:40:44 +0300

    Added crud methods on IScheduleService

--- EStudy/EStudy.Application/Interfaces/MVC/IScheduleService.cs
+++ EStudy/EStudy.Application/Interfaces/MVC/IScheduleService.cs
@@ -8,6 +8,30 @@ namespace EStudy.Application.Interfaces.MVC
 {
     public interface IScheduleService
     {
+        #region ScheduleCRUD
+        Task<string> AddScheduleAudience(ScheduleAudience model);
+        Task<string> EditScheduleAudience(ScheduleAudience model);
+        Task<string> RemoveScheduleAudience(int Id);
+        Task<string> AddScheduleDayOfWeek(ScheduleDayOfWeek model);
+        Task<string> EditScheduleDayOfWeek(ScheduleDayOfWeek model);
+        Task<string> RemoveScheduleDayOfWeek(int Id);
+        Task<string> AddScheduleDiscipline(ScheduleDiscipline model);
+        Task<string> EditScheduleDiscipline(ScheduleDiscipline model);
+        Task<string> RemoveScheduleDiscipline(int Id);
+        Task<string> AddScheduleGroup(ScheduleGroup model);
+        Task<string> EditScheduleGroup(ScheduleGroup model);
+        Task<string> RemoveScheduleGroup(int Id);
+        Task<string> AddScheduleLesson(ScheduleLesson model);
+        Task<string> EditScheduleLesson(ScheduleLesson model);
+        Task<string> RemoveScheduleLesson(int Id);
+        Task<string> AddScheduleParityOfWeek(ScheduleParityOfWeek model);
+        Task<string> EditScheduleParityOfWeek(ScheduleParityOfWeek model);
+        Task<string> RemoveScheduleParityOfWeek(int Id);
+        Task<string> AddScheduleTypeLesson(ScheduleTypeLesson model);
+        Task<string> EditScheduleTypeLesson(ScheduleTypeLesson model);
+        Task<string> RemoveScheduleTypeLesson(int Id);
+        #endregion
+
         Task<Schedule> GetScheduleById(long Id);
         Task<List<Schedule>> GetSchedulesByGroupId(long Id, DateTime date);
         Task<List<Schedule>> GetSchedulesByGroupIdOnWeek(long Id);
