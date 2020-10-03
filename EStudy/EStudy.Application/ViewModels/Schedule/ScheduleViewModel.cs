beae4bb030eefe080ee8a290cd3b5020ab6cde3e
@Yaroslav08 <yaroslav.mudryk@gmail.com>
2020-10-03 14:57:20 +0300

    Added ScheduleViewModel

---
+++ EStudy/EStudy.Application/ViewModels/Schedule/ScheduleViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace EStudy.Application.ViewModels.Schedule
+{
+    public class ScheduleViewModel
+    {
+        public long Id { get; set; }
+        public bool IsReplacement { get; set; }
+        public int? TeacherId { get; set; }
+        public DateTime DateLesson { get; set; }
+        public string Day { get; set; }
+        public string DayEng { get; set; }
+        public string ParityWeek { get; set; }
+        public byte Number { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public string GroupName { get; set; }
+        public string DisciplineName { get; set; }
+        public string ShortDisciplineName { get; set; }
+        public string DisciplineNameEng { get; set; }
+        public string LessonTypeName { get; set; }
+        public string LessonShortTypeName { get; set; }
+        public string LessonTypeNameEng { get; set; }
+        public string LessonShortTypeNameEng { get; set; }
+        public string NameAudience { get; set; }
+        public string NameAudienceEng { get; set; }
+    }
+}
