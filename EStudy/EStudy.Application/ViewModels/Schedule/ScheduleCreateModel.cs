4d3c2a39c83aa006f64a5ea671457c37a48ca62f
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-10-05 20:33:14 +0300

    Added Schedule create and edit model

---
+++ EStudy/EStudy.Application/ViewModels/Schedule/ScheduleCreateModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+namespace EStudy.Application.ViewModels.Schedule
+{
+    public class ScheduleCreateModel
+    {
+        [Required]
+        public bool IsReplacement { get; set; }
+        public int? TeacherId { get; set; }
+        [Required]
+        public DateTime DateLesson { get; set; }
+        public int ScheduleDayOfWeekId { get; set; }
+        public int ScheduleParityOfWeekId { get; set; }
+        public int ScheduleLessonId { get; set; }
+        public int ScheduleGroupId { get; set; }
+        public int ScheduleDisciplineId { get; set; }
+        public int ScheduleTypeLessonId { get; set; }
+        public int ScheduleAudienceId { get; set; }
+    }
+}
---
+++ EStudy/EStudy.Application/ViewModels/Schedule/ScheduleEditModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+namespace EStudy.Application.ViewModels.Schedule
+{
+    public class ScheduleEditModel
+    {
+        [Key]
+        public long Id { get; set; }
+        [Required]
+        public bool IsReplacement { get; set; }
+        public int? TeacherId { get; set; }
+        [Required]
+        public DateTime DateLesson { get; set; }
+        public int ScheduleDayOfWeekId { get; set; }
+        public int ScheduleParityOfWeekId { get; set; }
+        public int ScheduleLessonId { get; set; }
+        public int ScheduleGroupId { get; set; }
+        public int ScheduleDisciplineId { get; set; }
+        public int ScheduleTypeLessonId { get; set; }
+        public int ScheduleAudienceId { get; set; }
+    }
+}
