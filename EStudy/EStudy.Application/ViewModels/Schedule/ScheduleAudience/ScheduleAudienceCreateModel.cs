8dd20becc9b7cdd1d7c5c45180e6310f0b5831b2
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-10-05 20:09:03 +0300

    Added some files for create and edit ScheduleAudience

---
+++ EStudy/EStudy.Application/ViewModels/Schedule/ScheduleAudience/ScheduleAudienceCreateModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+namespace EStudy.Application.ViewModels.Schedule.ScheduleAudience
+{
+    public class ScheduleAudienceCreateModel
+    {
+        [Required, MinLength(1), MaxLength(25)]
+        public string NameAudience { get; set; }
+        [MinLength(1), MaxLength(25)]
+        public string NameAudienceEng { get; set; }
+    }
+}
