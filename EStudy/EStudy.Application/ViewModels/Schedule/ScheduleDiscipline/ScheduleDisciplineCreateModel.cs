5dfdbd4183f7aa2e7abbf848c6833c086140feae
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-10-05 20:16:23 +0300

    Added some files for create and edit ScheduleDiscipline

---
+++ EStudy/EStudy.Application/ViewModels/Schedule/ScheduleDiscipline/ScheduleDisciplineCreateModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+namespace EStudy.Application.ViewModels.Schedule.ScheduleDiscipline
+{
+    public class ScheduleDisciplineCreateModel
+    {
+        [Required, MinLength(3), MaxLength(100)]
+        public string DisciplineName { get; set; }
+        [MinLength(1), MaxLength(5)]
+        public string ShortDisciplineName { get; set; }
+        [MinLength(3), MaxLength(100)]
+        public string DisciplineNameEng { get; set; }
+        [MinLength(1), MaxLength(5)]
+        public string ShortDisciplineNameEng { get; set; }
+    }
+}
---
+++ EStudy/EStudy.Application/ViewModels/Schedule/ScheduleDiscipline/ScheduleDisciplineEditModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+namespace EStudy.Application.ViewModels.Schedule.ScheduleDiscipline
+{
+    public class ScheduleDisciplineEditModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MinLength(3), MaxLength(100)]
+        public string DisciplineName { get; set; }
+        [MinLength(1), MaxLength(5)]
+        public string ShortDisciplineName { get; set; }
+        [MinLength(3), MaxLength(100)]
+        public string DisciplineNameEng { get; set; }
+        [MinLength(1), MaxLength(5)]
+        public string ShortDisciplineNameEng { get; set; }
+    }
+}
