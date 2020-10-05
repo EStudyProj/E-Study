a52acdfc76bb03e07ae16a1a6c18dcba7ef93b85
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-10-05 20:11:25 +0300

    Added some files for create and edit ScheduleDayOfWeek

---
+++ EStudy/EStudy.Application/ViewModels/Schedule/ScheduleDayOfWeek/ScheduleDayOfWeekCreateModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+namespace EStudy.Application.ViewModels.Schedule.ScheduleDayOfWeek
+{
+    public class ScheduleDayOfWeekCreateModel
+    {
+        [Required, MinLength(3), MaxLength(25)]
+        public string Day { get; set; }
+        [MinLength(3), MaxLength(25)]
+        public string DayEng { get; set; }
+    }
+}
