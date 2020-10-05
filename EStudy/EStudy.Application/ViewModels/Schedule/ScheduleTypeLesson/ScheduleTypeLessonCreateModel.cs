296169f41bfdb3358f07758221f09e3a6a0cc795
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-10-05 20:29:13 +0300

    Added some files for create and edit ScheduleTypeLesson

---
+++ EStudy/EStudy.Application/ViewModels/Schedule/ScheduleTypeLesson/ScheduleTypeLessonCreateModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+namespace EStudy.Application.ViewModels.Schedule.ScheduleTypeLesson
+{
+    public class ScheduleTypeLessonCreateModel
+    {
+        [Required, MinLength(2), MaxLength(25)]
+        public string TypeName { get; set; }
+        [MinLength(1), MaxLength(5)]
+        public string ShortTypeName { get; set; }
+        [MinLength(2), MaxLength(25)]
+        public string TypeNameEng { get; set; }
+        [MinLength(1), MaxLength(5)]
+        public string ShortTypeNameEng { get; set; }
+    }
+}
---
+++ EStudy/EStudy.Application/ViewModels/Schedule/ScheduleTypeLesson/ScheduleTypeLessonEditModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+namespace EStudy.Application.ViewModels.Schedule.ScheduleTypeLesson
+{
+    public class ScheduleTypeLessonEditModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MinLength(2), MaxLength(25)]
+        public string TypeName { get; set; }
+        [MinLength(1), MaxLength(5)]
+        public string ShortTypeName { get; set; }
+        [MinLength(2), MaxLength(25)]
+        public string TypeNameEng { get; set; }
+        [MinLength(1), MaxLength(5)]
+        public string ShortTypeNameEng { get; set; }
+    }
+}
