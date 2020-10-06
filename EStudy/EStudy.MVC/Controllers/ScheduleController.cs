648f9463b18b86da5040cd08ec666aa9c2fb95f0
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-10-06 13:39:50 +0300

    Added ScheduleController and first view

---
+++ EStudy/EStudy.MVC/Controllers/ScheduleController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EStudy.Application.Interfaces.MVC;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EStudy.MVC.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        private ILogger<ScheduleController> logger;
+        private IScheduleService scheduleService;
+        public ScheduleController(ILogger<ScheduleController> _logger, IScheduleService _scheduleService)
+        {
+            logger = _logger;
+            scheduleService = _scheduleService;
+        }
+
+        [HttpGet("Schedule/{GroupId}")]
+        public async Task<IActionResult> GetScheduleOnWeekByGroupId(int GroupId)
+        {
+            return View(await scheduleService.GetSchedulesByGroupIdOnWeek(GroupId));
+        }
+    }
+}
