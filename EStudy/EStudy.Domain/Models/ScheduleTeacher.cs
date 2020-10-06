521a51e65f723e5c74119f4e4f6edf3de0c34284
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-10-06 13:54:18 +0300

    Added ScheduleTeacher entity

---
+++ EStudy/EStudy.Domain/Models/ScheduleTeacher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+namespace EStudy.Domain.Models
+{
+    public class ScheduleTeacher
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MinLength(5), MaxLength(100)]
+        public string Name { get; set; }
+        [MinLength(5), MaxLength(100)]
+        public string NameEng { get; set; }
+        public List<Schedule> Schedules { get; set; }
+
+
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? LastEdited { get; set; }
+    }
+}
