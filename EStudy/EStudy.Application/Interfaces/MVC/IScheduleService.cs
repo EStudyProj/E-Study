24a8b2a9dbe787f36d30a8758f7388b8c5bd79a0
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-10-07 20:17:33 +0300

    Added some methods to IScheduleService

--- EStudy/EStudy.Application/Interfaces/MVC/IScheduleService.cs
+++ EStudy/EStudy.Application/Interfaces/MVC/IScheduleService.cs
@@ -21,41 +21,49 @@ namespace EStudy.Application.Interfaces.MVC
         Task<string> AddScheduleAudience(ScheduleAudienceCreateModel model);
         Task<string> EditScheduleAudience(ScheduleAudienceEditModel model);
         Task<string> RemoveScheduleAudience(int Id);
+        Task<ScheduleAudienceEditModel> GetScheduleAudienceForEdit(int Id);
         Task<List<ScheduleAudienceViewModel>> GetAllScheduleAudiences();
 
         Task<string> AddScheduleDayOfWeek(ScheduleDayOfWeekCreateModel model);
         Task<string> EditScheduleDayOfWeek(ScheduleDayOfWeekEditModel model);
         Task<string> RemoveScheduleDayOfWeek(int Id);
+        Task<ScheduleDayOfWeekEditModel> GetScheduleDayOfWeekForEdit(int Id);
         Task<List<ScheduleDayOfWeekViewModel>> GetAllScheduleDayOfWeeks();
 
         Task<string> AddScheduleDiscipline(ScheduleDisciplineCreateModel model);
         Task<string> EditScheduleDiscipline(ScheduleDisciplineEditModel model);
         Task<string> RemoveScheduleDiscipline(int Id);
+        Task<ScheduleDisciplineEditModel> GetScheduleDisciplineForEdit(int Id);
         Task<List<ScheduleDisciplineViewModel>> GetAllScheduleDisciplines();
 
         Task<string> AddScheduleGroup(ScheduleGroupCreateModel model);
         Task<string> EditScheduleGroup(ScheduleGroupEditModel model);
         Task<string> RemoveScheduleGroup(int Id);
+        Task<ScheduleGroupEditModel> GetScheduleGroupEditForEdit(int Id);
         Task<List<ScheduleGroupViewModel>> GetAllScheduleGroups();
 
         Task<string> AddScheduleLesson(ScheduleLessonCreateModel model);
         Task<string> EditScheduleLesson(ScheduleLessonEditModel model);
         Task<string> RemoveScheduleLesson(int Id);
+        Task<ScheduleLessonEditModel> GetScheduleLessonForEdit(int Id);
         Task<List<ScheduleLessonViewModel>> GetAllScheduleLessons();
 
         Task<string> AddScheduleParityOfWeek(ScheduleParityOfWeekCreateModel model);
         Task<string> EditScheduleParityOfWeek(ScheduleParityOfWeekEditModel model);
         Task<string> RemoveScheduleParityOfWeek(int Id);
+        Task<ScheduleParityOfWeekEditModel> GetScheduleParityOfWeekForEdit(int Id);
         Task<List<ScheduleParityOfWeekViewModel>> GetAllScheduleParityOfWeeks();
 
         Task<string> AddScheduleTeacher(ScheduleTeacherCreateModel model);
         Task<string> EditScheduleTeacher(ScheduleTeacherEditModel model);
         Task<string> RemoveScheduleTeacher(int Id);
+        Task<ScheduleTeacherEditModel> GetScheduleTeacherForEdit(int Id);
         Task<List<ScheduleTeacherViewModel>> GetAllScheduleTeachers();
 
         Task<string> AddScheduleTypeLesson(ScheduleTypeLessonCreateModel model);
         Task<string> EditScheduleTypeLesson(ScheduleTypeLessonEditModel model);
         Task<string> RemoveScheduleTypeLesson(int Id);
+        Task<ScheduleTypeLessonEditModel> GetScheduleTypeLessonForEdit(int Id);
         Task<List<ScheduleTypeLessonViewModel>> GetAllScheduleTypeLessons();
         #endregion
 
