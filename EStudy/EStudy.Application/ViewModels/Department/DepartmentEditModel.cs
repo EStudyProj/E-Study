28b7f114d57e3c3f0f029b624c1bca026d6b0a73
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-10-12 20:10:21 +0300

    Added DepartmentEditModel

---
+++ EStudy/EStudy.Application/ViewModels/Department/DepartmentEditModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+namespace EStudy.Application.ViewModels.Department
+{
+    public class DepartmentEditModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MinLength(4), MaxLength(60)]
+        public string Name { get; set; }
+        [MinLength(4), MaxLength(60)]
+        public string InstituteName { get; set; }
+        [Required, MinLength(2), MaxLength(100)]
+        public string Shifr { get; set; }
+        [Required, Phone]
+        public string Phone { get; set; }
+        [MinLength(15), MaxLength(3000)]
+        public string ContactInformation { get; set; }
+        [Required, MinLength(10), MaxLength(300)]
+        public string Description { get; set; }
+    }
+}
